5c0589505806500de5f9808e8646887005252456
Mike Cheddar <kogcheddarxm@gmail.com>
2016-02-10 18:58:15 -0500

    Added styling, changed table layout, added time conversion to EST, displayed game times.

--- js/workflow/goal-buddy.js
+++ js/workflow/goal-buddy.js
@@ -15,6 +15,19 @@ $(document).ready(function(){
 		jQuery.each(currentGames, function(index, item){
 			//item.teams.away.team.name - gives the value of the name of the away team.
 			//item.teams.home.team.name - gives the value of the name of the home team.
+			var gameState = item.status.abstractGameState;
+			var gameStart = moment(item.gameDate)
+			//var clientDate = new Date();
+			//
+			var gameTime = gameStart.tz('America/New_York').format('h:mma z'); 
+			//var gameTime    = moment.tz(gameStart, "America/New_York");
+			//var date = new Date(((gameStart || "").replace(/-/g,"/").replace(/[TZ]/g," ")) + clientDate.getTimezoneOffset());
+
+			//alert(date.toLocaleTimeString());
+			//var gameStartArr = gameStart.split('T');
+
+			//var gameTime = gameStartArr[1];
+
 			var homeName = item.teams.home.team.name;
 			var awayName = item.teams.away.team.name;
 
@@ -23,13 +36,26 @@ $(document).ready(function(){
 
 			$("#scoreTable").append("<tr>");
 			$("#scoreTable").append("<td>" + homeName + "</td>");
+
+			if(gameState === "Live"){
+				$("#scoreTable").append("<td>" + homeScore + "</td>");
+				$("#scoreTable").append("<td>" + " - " + "</td>");
+				$("#scoreTable").append("<td>" + awayScore + "</td>");
+			}
+			else
+			{
+				$("#scoreTable").append("<td></td>");
+				$("#scoreTable").append("<td class='gameTimes'>" + gameTime + "</td>");
+				$("#scoreTable").append("<td></td>");
+			}
+
 			$("#scoreTable").append("<td>" + awayName + "</td>");
 			$("#scoreTable").append("</tr>");
 
-			$("#scoreTable").append("<tr>");
-			$("#scoreTable").append("<td>" + homeScore + "</td>");
-			$("#scoreTable").append("<td>" + awayScore + "</td>");
-			$("#scoreTable").append("</tr>");
+			// $("#scoreTable").append("<tr>");
+			// $("#scoreTable").append("<td>" + homeScore + "</td>");
+			// $("#scoreTable").append("<td>" + awayScore + "</td>");
+			// $("#scoreTable").append("</tr>");
 		});
 	});
 	
