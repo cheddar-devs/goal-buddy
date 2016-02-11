26ecbe3d4686e85ff0ec3b4d63c3b17a7dd089b7
Mike Cheddar <kogcheddarxm@gmail.com>
2016-02-11 00:28:24 -0500

    Added dynamic changing of period based on data returned. Tinkered with different stylings for background. Loaded what team a box represents based on id and used a son object to transform that to a background color.

--- js/workflow/goal-buddy.js
+++ js/workflow/goal-buddy.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 	var currentDate;
+	var teamBGs = {"teams": ["blank", "njd-bg", "nyi-bg", "nyr-bg", "phi-bg", "pit-bg", "bos-bg", "buf-bg", "mon-bg", "ott-bg", "tor-bg", "blank", "car-bg", "fla-bg", "tbl-bg", "wsh-bg", "chi-bg", "det-bg", "nsh-bg", "stl-bg", "cgy-bg", "col-bg", "edm-bg", "van-bg", "ana-bg", "dal-bg", "lak-bg", "blank", "sjs-bg", "cbj-bg", "min-bg", "blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank","blank", "win-bg", "ari-bg"] };
 
 	$.get("https://statsapi.web.nhl.com/api/v1/schedule?", function(data){
 		currentDate = data.dates[0].date;
@@ -15,31 +16,52 @@ $(document).ready(function(){
 		jQuery.each(currentGames, function(index, item){
 			//item.teams.away.team.name - gives the value of the name of the away team.
 			//item.teams.home.team.name - gives the value of the name of the home team.
+			var link = item.link;
+
+			var expandedData;
+			$.getJSON("https://statsapi.web.nhl.com/" + link, function(data){expandedData = data;}).done(function(data){
+				var curPeriod = expandedData.liveData.linescore.currentPeriod;
+			});
+			
 			var gameState = item.status.abstractGameState;
+			var gamePeriod = item.status.codedGameState;
 			var gameStart = moment(item.gameDate)
-			//var clientDate = new Date();
-			//
+
 			var gameTime = gameStart.tz('America/New_York').format('h:mma z'); 
-			//var gameTime    = moment.tz(gameStart, "America/New_York");
-			//var date = new Date(((gameStart || "").replace(/-/g,"/").replace(/[TZ]/g," ")) + clientDate.getTimezoneOffset());
-
-			//alert(date.toLocaleTimeString());
-			//var gameStartArr = gameStart.split('T');
-
-			//var gameTime = gameStartArr[1];
 
 			var homeName = item.teams.home.team.name;
 			var awayName = item.teams.away.team.name;
 
 			var homeScore = item.teams.home.score;
 			var awayScore = item.teams.away.score;
+			
+			var homeBG="";
+			var awayBG="";
+			
+			var homeId = item.teams.home.team.id;
+			var awayId = item.teams.away.team.id;
+
+			homeBG = teamBGs.teams[homeId];
+			awayBG = teamBGs.teams[awayId];
 
 			$("#scoreTable").append("<tr>");
-			$("#scoreTable").append("<td>" + homeName + "</td>");
+			$("#scoreTable").append("<td class='"+ homeBG +"'>" + homeName + "</td>");
 
 			if(gameState === "Live"){
 				$("#scoreTable").append("<td>" + homeScore + "</td>");
-				$("#scoreTable").append("<td>" + " - " + "</td>");
+				if(curPeriod === "1")
+					$("#scoreTable").append("<td>" + curPeriod + "st</td>");
+				if(curPeriod === "2")
+					$("#scoreTable").append("<td>" + curPeriod + "nd</td>");
+				if(curPeriod === "3")
+					$("#scoreTable").append("<td>" + curPeriod + "rd</td>");
+				if(curPeriod === "OT")
+					$("#scoreTable").append("<td>" + "OT" + "</td>");
+				$("#scoreTable").append("<td>" + awayScore + "</td>");
+			}
+			else if(gameState === "Final"){
+				$("#scoreTable").append("<td>" + homeScore + "</td>");
+				$("#scoreTable").append("<td>" + " Final " + "</td>");
 				$("#scoreTable").append("<td>" + awayScore + "</td>");
 			}
 			else
@@ -49,13 +71,10 @@ $(document).ready(function(){
 				$("#scoreTable").append("<td></td>");
 			}
 
-			$("#scoreTable").append("<td>" + awayName + "</td>");
+			$("#scoreTable").append("<td class='"+ awayBG +"'>" + awayName + "</td>");
 			$("#scoreTable").append("</tr>");
 
-			// $("#scoreTable").append("<tr>");
-			// $("#scoreTable").append("<td>" + homeScore + "</td>");
-			// $("#scoreTable").append("<td>" + awayScore + "</td>");
-			// $("#scoreTable").append("</tr>");
+
 		});
 	});
 	
