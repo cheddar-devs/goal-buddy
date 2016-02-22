982a1183dbff2d071957abc2f7c657ce5660f922
Mike Cheddar <kogcheddarxm@gmail.com>
2016-02-22 13:51:13 -0500

    Edited styling

--- js/workflow/directives.js
+++ js/workflow/directives.js
@@ -8,4 +8,19 @@ angular.module('goalBuddy.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).
+  directive('loading', function () {
+      return {
+        restrict: 'E',
+        replace:true,
+        template: '<div class="loading">LOADING...</div>',
+        link: function (scope, element, attr) {
+              scope.$watch('loading', function (val) {
+                  if (val)
+                      $(element).show();
+                  else
+                      $(element).hide();
+              });
+        }
+      }
+  });
